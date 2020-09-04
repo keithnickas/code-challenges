e7ccbfe2230974a7d3f2a173fb8eb039969973a8
Keith Nickas <keithnickas@gmail.com>
2020-09-03 23:42:02 -0700

    simple 2d array

---
+++ 2d-arrays/index.js
@@ -0,0 +1,394 @@
+const studentCoursePairs1 = [
+  ['58', 'Linear Algebra'],
+  ['94', 'Art History'],
+  ['94', 'Operating Systems'],
+  ['17', 'Software Design'],
+  ['58', 'Mechanics'],
+  ['58', 'Economics'],
+  ['17', 'Linear Algebra'],
+  ['17', 'Political Science'],
+  ['94', 'Economics'],
+  ['25', 'Economics'],
+  ['58', 'Software Design'],
+  ['58', 'Art History'],
+  ['94', 'Mechanics'],
+  ['19', 'Software Design'],
+  ['40', 'Mechanics'],
+  ['40', 'Economics'],
+  ['40', 'Linear Algebra'],
+  ['40', 'Political Science'],
+  ['19', 'Economics'],
+  ['17', 'Economics'],
+  ['20', 'Software Design'],
+  ['13', 'Linear Algebra'],
+  ['4', 'Art History'],
+  ['4', 'Operating Systems'],
+  ['106', 'Software Design'],
+  ['13', 'Mechanics'],
+  ['13', 'Economics'],
+  ['106', 'Linear Algebra'],
+  ['106', 'Political Science'],
+  ['4', 'Economics'],
+  ['3', 'Economics'],
+  ['13', 'Software Design'],
+  ['56', 'Art History'],
+  ['4', 'Mechanics'],
+  ['3', 'Software Design'],
+  ['106', 'Mechanics'],
+  ['40', 'Economics'],
+  ['56', 'Linear Algebra'],
+  ['56', 'Political Science'],
+  ['56', 'Economics'],
+  ['106', 'Economics'],
+  ['3', 'Software Design'],
+  ['58', 'Linear Algebra'],
+  ['94', 'Art History'],
+  ['94', 'Operating Systems'],
+  ['17', 'Software Design'],
+  ['58', 'Mechanics'],
+  ['58', 'Economics'],
+  ['17', 'Linear Algebra'],
+  ['17', 'Political Science'],
+  ['94', 'Economics'],
+  ['25', 'Economics'],
+  ['58', 'Software Design'],
+  ['58', 'Art History'],
+  ['94', 'Mechanics'],
+  ['19', 'Software Design'],
+  ['40', 'Mechanics'],
+  ['40', 'Economics'],
+  ['40', 'Linear Algebra'],
+  ['40', 'Political Science'],
+  ['19', 'Economics'],
+  ['17', 'Economics'],
+  ['20', 'Software Design'],
+  ['13', 'Linear Algebra'],
+  ['4', 'Art History'],
+  ['4', 'Operating Systems'],
+  ['106', 'Software Design'],
+  ['13', 'Mechanics'],
+  ['13', 'Economics'],
+  ['106', 'Linear Algebra'],
+  ['106', 'Political Science'],
+  ['4', 'Economics'],
+  ['3', 'Economics'],
+  ['13', 'Software Design'],
+  ['56', 'Art History'],
+  ['4', 'Mechanics'],
+  ['3', 'Software Design'],
+  ['106', 'Mechanics'],
+  ['40', 'Economics'],
+  ['56', 'Linear Algebra'],
+  ['56', 'Political Science'],
+  ['56', 'Economics'],
+  ['106', 'Economics'],
+  ['3', 'Software Design'],
+  ['58', 'Linear Algebra'],
+  ['94', 'Art History'],
+  ['94', 'Operating Systems'],
+  ['17', 'Software Design'],
+  ['58', 'Mechanics'],
+  ['58', 'Economics'],
+  ['17', 'Linear Algebra'],
+  ['17', 'Political Science'],
+  ['94', 'Economics'],
+  ['25', 'Economics'],
+  ['58', 'Software Design'],
+  ['58', 'Art History'],
+  ['94', 'Mechanics'],
+  ['19', 'Software Design'],
+  ['40', 'Mechanics'],
+  ['40', 'Economics'],
+  ['40', 'Linear Algebra'],
+  ['40', 'Political Science'],
+  ['19', 'Economics'],
+  ['17', 'Economics'],
+  ['20', 'Software Design'],
+  ['13', 'Linear Algebra'],
+  ['4', 'Art History'],
+  ['4', 'Operating Systems'],
+  ['106', 'Software Design'],
+  ['13', 'Mechanics'],
+  ['13', 'Economics'],
+  ['106', 'Linear Algebra'],
+  ['106', 'Political Science'],
+  ['4', 'Economics'],
+  ['3', 'Economics'],
+  ['13', 'Software Design'],
+  ['56', 'Art History'],
+  ['4', 'Mechanics'],
+  ['3', 'Software Design'],
+  ['106', 'Mechanics'],
+  ['40', 'Economics'],
+  ['56', 'Linear Algebra'],
+  ['56', 'Political Science'],
+  ['56', 'Economics'],
+  ['106', 'Economics'],
+  ['3', 'Software Design'],
+  ['58', 'Linear Algebra'],
+  ['94', 'Art History'],
+  ['94', 'Operating Systems'],
+  ['17', 'Software Design'],
+  ['58', 'Mechanics'],
+  ['58', 'Economics'],
+  ['17', 'Linear Algebra'],
+  ['17', 'Political Science'],
+  ['94', 'Economics'],
+  ['25', 'Economics'],
+  ['58', 'Software Design'],
+  ['58', 'Art History'],
+  ['94', 'Mechanics'],
+  ['19', 'Software Design'],
+  ['40', 'Mechanics'],
+  ['40', 'Economics'],
+  ['40', 'Linear Algebra'],
+  ['40', 'Political Science'],
+  ['19', 'Economics'],
+  ['17', 'Economics'],
+  ['20', 'Software Design'],
+  ['13', 'Linear Algebra'],
+  ['4', 'Art History'],
+  ['4', 'Operating Systems'],
+  ['106', 'Software Design'],
+  ['13', 'Mechanics'],
+  ['13', 'Economics'],
+  ['106', 'Linear Algebra'],
+  ['106', 'Political Science'],
+  ['4', 'Economics'],
+  ['3', 'Economics'],
+  ['13', 'Software Design'],
+  ['56', 'Art History'],
+  ['4', 'Mechanics'],
+  ['3', 'Software Design'],
+  ['106', 'Mechanics'],
+  ['40', 'Economics'],
+  ['56', 'Linear Algebra'],
+  ['56', 'Political Science'],
+  ['56', 'Economics'],
+  ['106', 'Economics'],
+  ['3', 'Software Design'],
+  ['58', 'Linear Algebra'],
+  ['94', 'Art History'],
+  ['94', 'Operating Systems'],
+  ['17', 'Software Design'],
+  ['58', 'Mechanics'],
+  ['58', 'Economics'],
+  ['17', 'Linear Algebra'],
+  ['17', 'Political Science'],
+  ['94', 'Economics'],
+  ['25', 'Economics'],
+  ['58', 'Software Design'],
+  ['58', 'Art History'],
+  ['94', 'Mechanics'],
+  ['19', 'Software Design'],
+  ['40', 'Mechanics'],
+  ['40', 'Economics'],
+  ['40', 'Linear Algebra'],
+  ['40', 'Political Science'],
+  ['19', 'Economics'],
+  ['17', 'Economics'],
+  ['20', 'Software Design'],
+  ['13', 'Linear Algebra'],
+  ['4', 'Art History'],
+  ['4', 'Operating Systems'],
+  ['106', 'Software Design'],
+  ['13', 'Mechanics'],
+  ['13', 'Economics'],
+  ['106', 'Linear Algebra'],
+  ['106', 'Political Science'],
+  ['4', 'Economics'],
+  ['3', 'Economics'],
+  ['13', 'Software Design'],
+  ['56', 'Art History'],
+  ['4', 'Mechanics'],
+  ['3', 'Software Design'],
+  ['106', 'Mechanics'],
+  ['40', 'Economics'],
+  ['56', 'Linear Algebra'],
+  ['56', 'Political Science'],
+  ['56', 'Economics'],
+  ['106', 'Economics'],
+  ['3', 'Software Design'],
+  ['58', 'Linear Algebra'],
+  ['94', 'Art History'],
+  ['94', 'Operating Systems'],
+  ['17', 'Software Design'],
+  ['58', 'Mechanics'],
+  ['58', 'Economics'],
+  ['17', 'Linear Algebra'],
+  ['17', 'Political Science'],
+  ['94', 'Economics'],
+  ['25', 'Economics'],
+  ['58', 'Software Design'],
+  ['58', 'Art History'],
+  ['94', 'Mechanics'],
+  ['19', 'Software Design'],
+  ['40', 'Mechanics'],
+  ['40', 'Economics'],
+  ['40', 'Linear Algebra'],
+  ['40', 'Political Science'],
+  ['19', 'Economics'],
+  ['17', 'Economics'],
+  ['20', 'Software Design'],
+  ['13', 'Linear Algebra'],
+  ['4', 'Art History'],
+  ['4', 'Operating Systems'],
+  ['106', 'Software Design'],
+  ['13', 'Mechanics'],
+  ['13', 'Economics'],
+  ['106', 'Linear Algebra'],
+  ['106', 'Political Science'],
+  ['4', 'Economics'],
+  ['3', 'Economics'],
+  ['13', 'Software Design'],
+  ['56', 'Art History'],
+  ['4', 'Mechanics'],
+  ['3', 'Software Design'],
+  ['106', 'Mechanics'],
+  ['40', 'Economics'],
+  ['56', 'Linear Algebra'],
+  ['56', 'Political Science'],
+  ['56', 'Economics'],
+  ['106', 'Economics'],
+  ['3', 'Software Design'],
+  ['58', 'Linear Algebra'],
+  ['94', 'Art History'],
+  ['94', 'Operating Systems'],
+  ['17', 'Software Design'],
+  ['58', 'Mechanics'],
+  ['58', 'Economics'],
+  ['17', 'Linear Algebra'],
+  ['17', 'Political Science'],
+  ['94', 'Economics'],
+  ['25', 'Economics'],
+  ['58', 'Software Design'],
+  ['58', 'Art History'],
+  ['94', 'Mechanics'],
+  ['19', 'Software Design'],
+  ['40', 'Mechanics'],
+  ['40', 'Economics'],
+  ['40', 'Linear Algebra'],
+  ['40', 'Political Science'],
+  ['19', 'Economics'],
+  ['17', 'Economics'],
+  ['20', 'Software Design'],
+  ['13', 'Linear Algebra'],
+  ['4', 'Art History'],
+  ['4', 'Operating Systems'],
+  ['106', 'Software Design'],
+  ['13', 'Mechanics'],
+  ['13', 'Economics'],
+  ['106', 'Linear Algebra'],
+  ['106', 'Political Science'],
+  ['4', 'Economics'],
+  ['3', 'Economics'],
+  ['13', 'Software Design'],
+  ['56', 'Art History'],
+  ['4', 'Mechanics'],
+  ['3', 'Software Design'],
+  ['106', 'Mechanics'],
+  ['40', 'Economics'],
+  ['56', 'Linear Algebra'],
+  ['56', 'Political Science'],
+  ['56', 'Economics'],
+  ['106', 'Economics'],
+  ['3', 'Software Design'],
+];
+
+const studentCoursePairs2 = [
+  ['42', 'Software Design'],
+  ['0', 'Advanced Mechanics'],
+  ['9', 'Art History'],
+];
+
+function findAndSortStudentCourses(array) {
+  const reverseValues = {};
+  let matchCourseToStudent = {};
+  let courses = [];
+  let matchedResults = {};
+
+  /**
+   * similar in functionality and result, but less code is required and removes a loop
+   * obviously this wouldn't be an ideal method if the expected structure were to include
+   * a third index value
+   */
+  array.forEach((innerArray) => {
+    /**
+     * we know the value of the indexies therefore we don't need another loop
+     * and can assign a variable to each
+     */
+    const student = innerArray[0];
+    const course = innerArray[1];
+
+    // if the student doesn't exist create it
+    !matchCourseToStudent[student] &&
+      (matchCourseToStudent = { ...matchCourseToStudent, [student]: [] });
+
+    // assign course to new array
+    !courses.includes(course) && courses.push(course);
+
+    matchedResults = { ...matchedResults, [course]: [] };
+
+    !matchCourseToStudent[student].includes(course) && matchCourseToStudent[student].push(course);
+  });
+
+  // Previous method before above
+  // for (let i = 0; i < array.length; i++) {
+  //   const innerArray = array[i];
+  //
+  //   if (!matchCourseToStudent[innerArray[0]]) {
+  //     matchCourseToStudent = { ...matchCourseToStudent, [innerArray[0]]: [] };
+  //   }
+
+  //   for (let h = 0; h < innerArray.length; h++) {
+  //     const course = innerArray[1];
+  //     const chkCourses = matchCourseToStudent[innerArray[0]];
+  //     if (!courses.includes(course)) courses.push(course);
+  //     if (!chkCourses.includes(course)) {
+  //       matchCourseToStudent[innerArray[0]].push(course);
+  //     }
+  //   }
+  // }
+
+  // could this be simplified or removed?
+  // courses.forEach((course) => {
+  //   matchedResults = { ...matchedResults, [course]: [] };
+  // });
+
+  Object.keys(matchCourseToStudent).forEach((student) => {
+    const id = student;
+    courses.forEach((course) => {
+      if (matchCourseToStudent[id].includes(course)) {
+        matchedResults[course].push(id);
+      }
+    });
+  });
+
+  // since we want the studen ids first this will flip the key value pairs
+  Object.keys(matchedResults).forEach((key) => {
+    reverseValues[matchedResults[key]] = key;
+  });
+
+  return reverseValues;
+}
+
+findAndSortStudentCourses(studentCoursePairs1);
+findAndSortStudentCourses(studentCoursePairs2);
+
+/**
+ * expected output:
+ * {
+ * '13,17,40,56,58,106': 'Linear Algebra',
+ * '4,56,58,94': 'Art History',
+ * '4,94': 'Operating Systems',
+ * '3,13,17,19,20,58,106': 'Software Design',
+ * '4,13,40,58,94,106': 'Mechanics',
+ * '3,4,13,17,19,25,40,56,58,94,106': 'Economics',
+ * '17,40,56,106': 'Political Science'
+ * }
+ * {
+ * '0': 'Advanced Mechanics',
+ * '9': 'Art History',
+ * '42': 'Software Design'
+ * }
+ */
